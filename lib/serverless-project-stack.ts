0288a418d6ac11ad381f5581d7e2d362ddf959a9
Texpa <texpa@192.168.68.107>
2024-07-17 17:07:26 -0300

    primeiro commit

--- lib/serverless-project-stack.ts
+++ lib/serverless-project-stack.ts
@@ -1,16 +1,28 @@
 import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as lambda from 'aws-cdk-lib/aws-lambda'
+
+import * as apigw from 'aws-cdk-lib/aws-apigateway'
+import { HitCounter } from './hitcounter';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class ServerlessProjectStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const hello = new lambda.Function(this, 'HelloHandler', {
+      runtime: lambda.Runtime.NODEJS_16_X,
+      code: lambda.Code.fromAsset('lambda'),
+      handler: 'hello.handler'
+    })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'ServerlessProjectQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const helloWithCounter = new HitCounter(this, 'HelloHitCounter', {
+      downstream: hello
+    });
+
+    // defines an API Gateway REST API resource backed by our "hello" function.
+    new apigw.LambdaRestApi(this, 'Endpoint', {
+      handler: helloWithCounter.handler
+    });
+
   }
 }
